502cc3cafadf8019448ee3c2a3c4e58dcf707ce5
JamshidbekJS <jamshidbekml@gmail.com>
2021-09-23 00:10:11 +0500

    added remove button

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var add = document.getElementById('add');
 var removeFirst = document.getElementById('removeFirst');
 var removeLast = document.getElementById('removeLast');
 var schedule = document.getElementById('schedule');
+var input = document.getElementById('input');
 
 // ================ Add todo =======================================
 
@@ -10,19 +11,18 @@ add.addEventListener('click', function(e){
     if(document.getElementById('input').value == ''){       
         alert('Qatorga belgi kiritilishi shart!')       
     } else{
-        todos.push(document.getElementById('input').value);
-        var indexOfTodo = Number(todos.indexOf(document.getElementById('input').value))
-        // document.getElementById('schedule').textContent += a + 1 + ' - ' + indexOfTodo + '; ';
-        
-        // if(indexOfTodo == 0) ++indexOfTodo
-        
-        schedule.innerHTML += `<div id='todo${indexOfTodo+1}'>${indexOfTodo + 1}. ${todos[indexOfTodo]}<div>`
-
-        console.log(todos)
-
+        todos.push(input.value);
+        var indexOfTodo = Number(todos.indexOf(input.value));
+        var NEW_DIV = document.createElement('div');
+        NEW_DIV.innerHTML = indexOfTodo + 1 +'. '+ todos[indexOfTodo];
+        NEW_DIV.classList.add('fs-3','fw-bold');
+        NEW_DIV.setAttribute('id', 'todo'+ Number(indexOfTodo + 1))
+        schedule.appendChild(NEW_DIV);
         document.getElementById('input').value = '';
     }
 })
+
+var x = todos.length;
 
 // ================== Remove todo from last =======================
 
@@ -30,19 +30,8 @@ removeLast.addEventListener('click', function(e){
     if(todos == ''){       
         alert('Hech qanday amal kiritilmagan')       
     } else{
-        todos.pop(document.getElementById('input').value);
-        console.log(todos);
+        var a = todos.length;
+        todos.pop();
+        document.getElementById('todo'+ a).remove();
     }
-})
-
-// ================ Remove todo from first =========================
-
-removeFirst.addEventListener('click', function(e){
-    if(todos == ''){       
-        alert('Hech qanday amal kiritilmagan!')       
-    } else{
-        todos.shift(document.getElementById('input').value);
-        console.log(todos) 
-    }
-    
 })
